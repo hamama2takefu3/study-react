bc314d8612f8beb843735ac114aa9108edb094e3
hamama2takefu3 <60927351+hamama2takefu3@users.noreply.github.com>
2023-02-26 23:08:16 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/**
+ * const, let1nadono などの変数宣言
+ */
+// var val1 = "abcde";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "fghij";
+// console.log(val1);
+
+// // ｖａｒ変数は再宣言可能
+// var val1 = "klmn";
+// console.log(val1);
+
+// let val2 = "abcde";
+// console.log(val2);
+
+// val2 = "fghij";
+// console.log(val2);
+
+// let val2 = "klmn";
+
+// const val3 = "abcde";
+// console.log(val3);
+
+// val3 = "fghij";
+// const val3 = "klmn";
+
+// const val4 = {
+//   name: "aaa",
+//   age: 28
+// };
+// val4.name = "bbb";
+// val4.age = 14;
+// val4.adress = "@@@";
+
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
